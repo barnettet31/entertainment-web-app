973b52239be8ffd7adedc6880a30112cf7cad878
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2023-01-30 21:27:59 -0500

    working on adding progress bars and the different layouts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,20 @@
-import { type AppType } from "next/app";
+import { ReactElement, ReactNode } from "react";
+import { NextPage } from "next";
+import { AppProps, type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-
 import { api } from "../utils/api";
 
 import "../styles/globals.css";
 
+
+export type NextPageWithLayout<P = unknown> = NextPage<P> & {
+  getLayout?:(page:ReactElement) =>ReactNode;
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component:NextPageWithLayout;
+}
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
