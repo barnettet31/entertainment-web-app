8d4b3a252e3b9ac61bacae80cf52b358ec90af50
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2023-02-01 20:53:14 -0500

    added login button to login page

--- src/components/layouts/landingLayout/landingLayout.tsx
+++ src/components/layouts/landingLayout/landingLayout.tsx
@@ -2,24 +2,28 @@ import { ReactElement } from "react";
 import { LandingNav } from "./landingNav";
 import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
+import { LandingFooter } from "./landingFooter";
+import { LoadingPage } from "../../loadingPage/loadingPage.component";
 
 interface IProps {
   children: ReactElement;
 }
  const LandingLayout = ({ children }:IProps) => {
   const router = useRouter();
-  const { data: sessionData } = useSession();
+  const { data: sessionData, status } = useSession();
   if (sessionData)
     router
       .push("/dashboard")
       .then((d) => console.log(d))
       .catch((e) => console.log(e));
+  if(status === 'loading') return <LoadingPage/>
   return (
     <>
       <LandingNav />
-      <main className="w-full px-3 sm:pb-8 md:px-0">
-        <div className="container mx-auto sm:px-6 lg:px-8">{children}</div>
+      <main className="w-full px-3 sm:pb-10 md:px-0">
+        <div className="container mx-auto sm:px-6 lg:px-8 mb-8">{children}</div>
       </main>
+      <LandingFooter/>
     </>
   );
 };
--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,10 +1,21 @@
+import { signIn } from "next-auth/react";
 import { getLandingLayout } from "../../components/layouts/landingLayout/landingLayout";
 import { NextPageWithLayout } from "../_app";
 
-const SignIn:NextPageWithLayout = ()=>{
-    return <h1 className="text-red-600 text-3xl font-bold">This is my login page</h1>
-}
+const SignIn: NextPageWithLayout = () => {
+  return (
+    <div className="grid h-screen place-items-center">
+      <button
+        type="button"
+        onClick={() => void signIn("discord")}
+        className="inline-flex items-center rounded-sm border border-transparent bg-red px-6 py-3 text-center text-lg font-medium text-white shadow-sm hover:bg-red/70 md:w-1/4"
+      >
+       Login with Discord
+      </button>
+    </div>
+  );
+};
 
-SignIn.getLayout = getLandingLayout
+SignIn.getLayout = getLandingLayout;
 
-export default SignIn
+export default SignIn;
