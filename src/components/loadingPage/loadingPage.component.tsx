a10f6fb7524c193f1c14d4bfe3e8e702324cf056
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2023-02-03 11:28:38 -0500

    fixed layout of loading page

--- src/components/loadingPage/loadingPage.component.tsx
+++ src/components/loadingPage/loadingPage.component.tsx
@@ -1,8 +1,10 @@
-export const LoadingPage =()=>{
-    return (
-      <div className="grid h-screen w-screen place-items-center">
-        <div className="border-red h-32 w-32 animate-spin rounded-full border-8 border-r-transparent"></div>
-        <h1 className="text-red text-4xl font-bold text-center">Loading...</h1>
+export const LoadingPage = () => {
+  return (
+    <div className="grid h-screen w-screen place-items-center">
+      <div className="flex flex-col gap-4">
+        <div className="h-32 w-32 animate-spin rounded-full border-8 border-red border-r-transparent"></div>
+        <h1 className="text-center text-4xl font-bold text-red">Loading...</h1>
       </div>
-    );
-}
+    </div>
+  );
+};
