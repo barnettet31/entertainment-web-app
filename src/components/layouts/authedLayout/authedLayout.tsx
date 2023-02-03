a58c66151538823a48f2dccb1ab0306dec6ccd0a
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2023-02-03 11:29:00 -0500

    added session validation to dashboard layout instead of index

--- src/components/layouts/authedLayout/authedLayout.tsx
+++ src/components/layouts/authedLayout/authedLayout.tsx
@@ -1,11 +1,19 @@
-import { ReactElement } from "react";
-import { AuthNav } from "./authNav";
+import { ReactElement, useEffect } from "react";
+import {  AuthHeader } from "../../authHeader/authHeader.component";
+import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
 
 interface IProps {
   children: ReactElement;
 }
 const AuthedLayout = ({ children }: IProps) => {
-  return <><AuthNav/>{children}</>;
+    const router = useRouter();
+    const { data: sessionData } = useSession();
+
+    useEffect(() => {
+      if (!sessionData) router.push("/").catch(console.log);
+    }, [sessionData]);
+  return <><AuthHeader/>{children}</>;
 };
 export const getAuthedLayout = (page: ReactElement) => {
   return <AuthedLayout>{page}</AuthedLayout>;
