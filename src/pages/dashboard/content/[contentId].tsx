8c8bc43f2a999676005fe29d1006a0199d87e985
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2023-02-07 11:16:19 -0500

    working on importing data to the content page

--- src/pages/dashboard/content/[contentId].tsx
+++ src/pages/dashboard/content/[contentId].tsx
@@ -1,12 +1,16 @@
 import { useRouter } from "next/router";
 import { getAuthedLayout } from "../../../components/layouts/authedLayout/authedLayout";
 import type { NextPageWithLayout } from "../../_app";
+import { api } from "../../../utils/api";
+import { ErrorPage } from "../../../components/errorPage/errorPage.component";
 const ContentPage: NextPageWithLayout = () => {
   const router = useRouter();
   const {contentId} = router.query;
-  console.log(router.query.contentId)
-  console.log(contentId)
-  return <h1>{contentId}</h1>
+  if(!contentId || typeof contentId === 'object') return <ErrorPage/>
+  const {data} = api.movies.getContentById.useQuery({id:contentId}, {
+    enabled: contentId ? true:false 
+  })
+  return <h1>{JSON.stringify(data)}</h1>
 };
 ContentPage.getLayout = getAuthedLayout;
 export default ContentPage;
